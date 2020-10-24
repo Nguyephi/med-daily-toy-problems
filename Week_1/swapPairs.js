d233a8e7c9264d15cea654917fc7667ec62f9252
Phil <pronewgen92@gmail.com>
2020-10-24 14:58:58 -0400

    swapPairs

---
+++ Week_1/swapPairs.js
@@ -0,0 +1,29 @@
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+let linkedList = new ListNode(1)
+linkedList.next = new ListNode(2)
+linkedList.next.next = new ListNode(3)
+linkedList.next.next.next = new ListNode(4)
+
+var swapPairs = function (head) {
+    // base case
+    if (head === null || head.next === null) return head;
+
+    // get the first and second node of linkedList
+    let firstNode = head;
+    let secondNode = head.next;
+
+    // I want the first node to be swapped with the second node, while calling it recursively to get the swapped list of subsequent nodes
+    firstNode.next = swapPairs(secondNode.next);
+    // Now swap the second node of each pairs with the first node
+    secondNode.next = firstNode;
+
+    // by returning the second node every time it returns it is returning the swapped pairs until it reaches the first pair
+    return secondNode
+};
+
+console.log(swapPairs(linkedList))
